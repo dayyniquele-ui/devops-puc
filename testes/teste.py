cbfeac188d4494d9dd2f56fdbcede1045ee4f9e8
dayyniquele-ui <dayy.niquele@gmail.com>
2026-04-23 23:14:55 -0300

    teste2

--- testes/teste.py
+++ testes/teste.py
@@ -3,21 +3,31 @@ from unittest.mock import patch
 
 
 def teste_root():
-    assert root() == {"message": "Hello World"}
+    result = root()
+    yield result
+    assert result() == {"message": "Hello World"}
   
 def teste_funcaoteste2 ():
     with patch('src.main.random.randint', return_value=42):
         result = funcaoteste2() 
+        yield result
     assert result == {"teste1": True, "num_aleatorio": 42}
 
 def teste_create_estudante():
     estudante_teste = Estudante(id=1, nome="Dayanna", curso="ADS", idade=35)
-    assert estudante_teste == create_estudante(estudante_teste)
+    result = create_estudante(estudante_teste)
+    yield result
+
+    assert estudante_teste == result
 
 def teste_update_estudante():
     estudante_teste = Estudante(id=1, nome="Dayanna", curso="ADS", idade=35)
-    assert estudante_teste == update_estudante(1, estudante_teste)
+    result = update_estudante(1, estudante_teste)
+    yield result
+    assert estudante_teste == result
 
 def teste_delete_estudante():
-    assert delete_estudante(1) == {"estudante_id": 1, "message": "Estudante deletado com sucesso"}
+    result = delete_estudante(1)
+    yield result
+    assert result == {"estudante_id": 1, "message": "Estudante deletado com sucesso"}
 

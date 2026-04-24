0e8a47b12076c940b748e38a4ff16c8197c85b59
dayyniquele-ui <dayy.niquele@gmail.com>
2026-04-23 23:44:33 -0300

    finalboss

--- testes/test.py
+++ testes/test.py
@@ -1,27 +1,36 @@
+import pytest
 from src.main import *
 from unittest.mock import patch
 
 
-def test_root():
-    result = root()
+@pytest.mark.asyncio
+async def test_root():
+    result = await root()
     assert result == {"message": "Hello World"}
 
 
-def test_funcaoteste2():
+@pytest.mark.asyncio
+async def test_funcaoteste2():
     with patch('src.main.random.randint', return_value=42):
-        result = funcaoteste2()
+        result = await funcaoteste2()
     assert result == {"teste1": True, "num_aleatorio": 42}
 
-def test_create_estudante():
+
+@pytest.mark.asyncio
+async def test_create_estudante():
     estudante_teste = Estudante(id=1, nome="Dayanna", curso="ADS", idade=35)
-    result = create_estudante(estudante_teste)
+    result = await create_estudante(estudante_teste)
     assert estudante_teste == result
 
-def test_update_estudante():
+
+@pytest.mark.asyncio
+async def test_update_estudante():
     estudante_teste = Estudante(id=1, nome="Dayanna", curso="ADS", idade=35)
-    result = update_estudante(1, estudante_teste)
+    result = await update_estudante(1, estudante_teste)
     assert estudante_teste == result
 
-def test_delete_estudante():
-    result = delete_estudante(1)
+
+@pytest.mark.asyncio
+async def test_delete_estudante():
+    result = await delete_estudante(1)
     assert result == {"estudante_id": 1, "message": "Estudante deletado com sucesso"}
